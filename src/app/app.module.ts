0027d03760dea0a5f0ce145a0f74868fe95df8a5
Maciej Paluch <maciekpal@gmail.com>
2022-09-09 10:22:34 +0200

    Excercise 4 done: Using new API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
 import {EmployeeFacesComponentModule} from "./ui/employee-faces/employee-faces.component-module";
-import {EmployeeService} from "./services/employee.service";
 import {EmployeeServiceModule} from "./services/employee.service-module";
 
 @NgModule({
