e13122f9f1f3f7cbc683feadf19354056806f0b8
Maciej Paluch <maciekpal@gmail.com>
2022-09-12 19:02:04 +0200

    Excercise pre 3: Added observable for posting data to the dummy server

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../../services/employee.service';
 
 @Component({
   selector: 'employee-form',
@@ -10,11 +11,17 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup({
     name: new FormControl(null, [Validators.required]),
-    age: new FormControl(null,[Validators.required,Validators.min(0)]),
-    salary: new FormControl(null, [Validators.required,Validators.min(0)])
+    age: new FormControl(null, [Validators.required, Validators.min(0)]),
+    salary: new FormControl(null, [Validators.required, Validators.min(0)])
   });
 
-  onButtonClicked(form: {name: string,age: number,salary: number}){
-    alert('User was successfully added to the database! Name: '+form.name+', age: '+form.age+', salary: '+form.salary);
+  constructor(private _employeeService: EmployeeService) {
+  }
+
+  onButtonClicked(form: { name: string, age: number, salary: number }) {
+    alert('User was successfully added to the database! Name: ' + form.name + ', age: ' + form.age + ', salary: ' + form.salary);
+  }
+  onFormSubmitted(form: { name: string, age: number, salary: number }) {
+    this._employeeService.create(form).subscribe();
   }
 }
