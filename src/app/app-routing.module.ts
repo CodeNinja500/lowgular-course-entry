5a76aec5907fe1c3fb8c9d3eee6976ed32c3d96d
Maciej Paluch <maciekpal@gmail.com>
2022-09-29 21:40:40 +0200

    Part 4 - Exercise 1 Created 3 views for home page, form and list.
    Still working on the styling.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./ui/home/home.component";
+import {HomeComponentModule} from "./ui/home/home.component-module";
+import {EmployeeListComponent} from "./ui/employee-list/employee-list.component";
+import {EmployeeListComponentModule} from "./ui/employee-list/employee-list.component-module";
+import {EmployeeFormComponent} from "./ui/employee-form/employee-form.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+  },
+  {
+    path: 'employee-list',
+    component: EmployeeListComponent
+  },
+  {
+    path: 'create-employee',
+    component: EmployeeFormComponent
+  }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),HomeComponentModule,EmployeeListComponentModule,EmployeeListComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/ui/home/home.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeComponent {
+
+  changeView(){
+    console.log('clicked');
+  }
+
+}
